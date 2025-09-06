7f2a5e479cf56c9215ead60a075f709834d37a07
arsalan <marsalan318@gmail.com>
2025-09-06 23:43:57 +0500

    new feature

---
+++ resources/views/feature.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1>Feature Page</h1>
+    </header>
+    <main>
+        <p>This is the feature page content.</p>
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Description</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Feature One</td>
+                    <td>This is the first feature.</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Feature Two</td>
+                    <td>This is the second feature.</td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td>Feature Three</td>
+                    <td>This is the third feature.</td>
+                </tr>
+            </tbody>
+    </main>
+    <footer>
+        <p>&copy; 2024 My Application</p>
+    </footer>
+</body>
+</html>
